94093508d4e94cd4f387b457679ca3487f2c35db
unknown <mbisovskyi@gmail.com>
2022-08-09 10:03:59 -0400

    DisplayMusic more styles added + .svg likes icon added

--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -14,7 +14,30 @@ const DisplayMusic = ({ NewMusic }) => {
           <div className="display-music-item">Genre</div>
         </div>
       </div>
-      <div className="likes-container">Likes:</div>
+      <div className="likes-container">
+        17
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="29.803"
+            height="27.326"
+            viewBox="0 0 29.803 27.326"
+          >
+            <g
+              id="Group_14"
+              data-name="Group 14"
+              transform="translate(0 -4.634)"
+            >
+              <path
+                id="iconmonstr-favorite-5"
+                d="M14.981,83.908C11.471,76.836.08,78.862.08,87.969c0,9.028,12.3,13.583,14.9,19.014,2.6-5.431,14.9-9.986,14.9-19.014C29.883,78.87,18.5,76.827,14.981,83.908Z"
+                transform="translate(-0.08 -75.023)"
+                fill="#ffffff"
+              />
+            </g>
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
