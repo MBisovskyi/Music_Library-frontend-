88296ee0b04bcaf8c65d80b5c0753bb71b71932f
unknown <mbisovskyi@gmail.com>
2022-08-09 15:16:42 -0400

    Functionality of getAllSongs is working properly + Some fixes for styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import SearchField from "./Components/SearchField/SearchField";
 import DisplayMusic from "./Components/DisplayMusic/DisplayMusic";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [songs]);
+
+  async function getAllSongs() {
+    let response = await axios.get("http://127.0.0.1:8000/songs/");
+    setSongs(response.data);
+    console.log(response);
+  }
+
   return (
     <div className="app">
       <NavBar />
       <SearchField />
-      <DisplayMusic />
+      <DisplayMusic allSongs={songs} />
     </div>
   );
 }
--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -1,43 +1,43 @@
 import "./DisplayMusic.css";
 
-const DisplayMusic = ({ NewMusic }) => {
+const DisplayMusic = (props) => {
   return (
-    <div className="display-music-wrap">
-      <div className="music-title">Title</div>
-      <div className="music-content-container">
-        <div className="display-music-left-container">
-          <div className="display-music-item">Artist/Band:</div>
-          <div className="display-music-item">Release Date:</div>
-        </div>
-        <div className="display-music-right-container">
-          <div className="display-music-item">Album</div>
-          <div className="display-music-item">Genre</div>
-        </div>
-      </div>
-      <div className="likes-container">
-        <div>17</div>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="29.803"
-            height="27.326"
-            viewBox="0 0 29.803 27.326"
-          >
-            <g
-              id="Group_14"
-              data-name="Group 14"
-              transform="translate(0 -4.634)"
-            >
-              <path
-                id="iconmonstr-favorite-5"
-                d="M14.981,83.908C11.471,76.836.08,78.862.08,87.969c0,9.028,12.3,13.583,14.9,19.014,2.6-5.431,14.9-9.986,14.9-19.014C29.883,78.87,18.5,76.827,14.981,83.908Z"
-                transform="translate(-0.08 -75.023)"
-                fill="#ffffff"
-              />
-            </g>
-          </svg>
-        </button>
-      </div>
+    <div className="content-wrap">
+      {props.allSongs.map((song, index) => {
+        return (
+          <div className="display-music-wrap" key={index}>
+            <p className="music-title">{song.title}</p>
+            <div className="music-content-conteiner">
+              <div className="display-music-item">
+                <p>Artist/Band: {song.artist}</p>
+                <p>Album: {song.album}</p>
+              </div>
+              <div className="display-music-item">
+                <p>Date Released: {song.release_date}</p>
+                <p>Genre: {song.genre}</p>
+              </div>
+            </div>
+            <div className="likes-container">
+              {song.likes_quantity}
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25.803"
+                  height="23.326"
+                  viewBox="0 0 29.803 27.326"
+                >
+                  <path
+                    id="iconmonstr-favorite-5"
+                    d="M14.981,83.908C11.471,76.836.08,78.862.08,87.969c0,9.028,12.3,13.583,14.9,19.014,2.6-5.431,14.9-9.986,14.9-19.014C29.883,78.87,18.5,76.827,14.981,83.908Z"
+                    transform="translate(-0.08 -79.657)"
+                    fill="#846cd7"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
