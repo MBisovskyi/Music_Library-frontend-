8eb66fa23ef15afdd9aeeea2cbcfa3d9e40564dc
unknown <mbisovskyi@gmail.com>
2022-08-10 12:54:57 -0400

    Songs filter thru Album prompt is completed

--- src/Components/SearchField/SearchField.jsx
+++ src/Components/SearchField/SearchField.jsx
@@ -4,10 +4,20 @@ import axios from "axios";
 const SearchField = (props) => {
   async function filterByArtist() {
     let response = await axios.get("http://127.0.0.1:8000/songs/");
-    console.log(response);
     let artistInput = prompt("Please, enter Artist/Band name: ");
     let songsArray = response.data.filter(function (song) {
       if (song.artist === artistInput) {
+        return true;
+      }
+    });
+    props.newSongs(songsArray);
+  }
+
+  async function filterByAlbum() {
+    let response = await axios.get("http://127.0.0.1:8000/songs/");
+    let albumInput = prompt("Please, enter Album name: ");
+    let songsArray = response.data.filter(function (song) {
+      if (song.album === albumInput) {
         return true;
       }
     });
@@ -20,7 +30,7 @@ const SearchField = (props) => {
         <button onClick={filterByArtist}>artist/band</button>
       </div>
       <div className="search-field-item">
-        <button>album</button>
+        <button onClick={filterByAlbum}>album</button>
       </div>
       <div className="search-field-item">
         <button>release date</button>
